acb7d915c79addc48bab475bc0f5c44a61e2a5ce
jobayer <jobayer@iastate.edu>
2022-05-12 12:36:37 -0500

    [DSA] Updated Quick Sort

--- dsa/quicksort.py
+++ dsa/quicksort.py
@@ -1,28 +1,34 @@
 from typing import List
 
 
-def quick_sort(arr: List[int], left: int, right: int) -> None:
-    i, j = left, right
+def quick_sort(arr: List[int]) -> None:
+    if len(arr) < 2: 
+        return
 
-    # partition
-    while i <= j:
+    def sort(arr: List[int], left: int, right: int):
+        i, j = left, right
         pivot = arr[left + (right - left)//2]
-        while arr[i] < pivot:
-            i += 1
-        while arr[j] > pivot:
-            j -= 1
-        if i <= j:
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-            j -= 1
-            
-    # recursion
-    if left < j:
-        quick_sort(arr, left, j)
-    if i < right:
-        quick_sort(arr, i, right)
+
+        # partition
+        while i <= j:
+            while arr[i] < pivot:
+                i += 1
+            while arr[j] > pivot:
+                j -= 1
+            if i <= j:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+
+        # recursion
+        if left < j:
+            sort(arr, left, j)
+        if i < right:
+            sort(arr, i, right)
+    
+    sort(arr, 0, len(arr) - 1)
 
 		
-array = [10, 7, 8, 9, 1, 5]
-quick_sort(array, 0, len(array) - 1)
+array = [1,2,2,1,1,2]
+quick_sort(array)
 print(f'Sorted array: {array}')
